cd811cb794cd2a311d3de17ac3c3655b1903b3a8
Dominique Stender <dominique.stender@gmail.com>
2018-12-08 12:57:25 +0100

    more improvements on the sequence characters to identify the cursor better on current steps

--- arduino_euclid_sequencer/display_customChars.h
+++ arduino_euclid_sequencer/display_customChars.h
@@ -43,41 +43,41 @@ byte display_charArrowLeft[8] = {
 };
 byte display_charStepActive[8] = {
   0b00000,
-  0b00000,
   0b00100,
   0b01110,
   0b11111,
   0b01110,
   0b00100,
+  0b00000,
   0b00000
 };
 byte display_charStepActiveCurrent[8] = {
-  0b11111,
-  0b11111,
+  0b00000,
   0b11011,
   0b10001,
   0b00000,
   0b10001,
   0b11011,
-  0b11111
+  0b00000,
+  0b00000
 };
 byte display_charStepInactive[8] = {
-  0b00000,
   0b00000,
   0b00000,
   0b00100,
   0b01010,
   0b00100,
   0b00000,
+  0b00000,
   0b00000
 };
 byte display_charStepInactiveCurrent[8] = {
-  0b11111,
-  0b11111,
+  0b00000,
   0b11111,
   0b11011,
   0b10101,
   0b11011,
   0b11111,
-  0b11111
+  0b00000,
+  0b00000
 };
