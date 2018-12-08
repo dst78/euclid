41fbf51f31a96e4aa0b6e03fc37cb86db727ef22
Dominique Stender <dominique.stender@gmail.com>
2018-12-08 23:49:38 +0100

    added skeleton function for euclidian rythms

---
+++ arduino_euclid_sequencer/euclid.ino
@@ -0,0 +1,12 @@
+/**
+ * the euclidian rythm generator
+ * 
+ * the implementations I found online either generate incorrect patterns 
+ * or plainly didn't work alltogether so I wrote my own...
+ */
+
+uint32_t euclid(uint8_t len, uint8_t pulses, uint8_t offsets) {
+  uint32_t pattern = 0;
+
+  return pattern;
+}
